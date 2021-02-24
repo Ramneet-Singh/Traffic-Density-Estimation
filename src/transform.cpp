a34589521bc2bd8bffc93238888451b38f8da85f
rakshitachoudhary <58719819+rakshitachoudhary@users.noreply.github.com>
2021-02-24 23:15:09 +0530

    Update transform.cpp

--- src/transform.cpp
+++ src/transform.cpp
@@ -3,32 +3,27 @@
 using namespace cv;
 using namespace std;
 
-void transform(string &img_name, Mat &im_src, vector<Point2f> &pts_src)
+void transform(string &img_name, Mat &src_img, vector<Point2f> &src_vec)
 {
-    // Destination image
     Size size(328, 778);
-    Mat im_dst = Mat::zeros(size, CV_8UC3);
+    Mat dst_img = Mat::zeros(size, CV_8UC3);
+    vector<Point2f> dst_vec;
 
-    // Create a vector of points.
-    vector<Point2f> pts_dst;
-
-    pts_dst.push_back(Point2f(0, 0));
-    pts_dst.push_back(Point2f(size.width - 1, 0));
-    pts_dst.push_back(Point2f(size.width - 1, size.height - 1));
-    pts_dst.push_back(Point2f(0, size.height - 1));
-
-    //Create a window
+    dst_vec.push_back(Point2f(0, 0));
+    dst_vec.push_back(Point2f(size.width - 1, 0));
+    dst_vec.push_back(Point2f(size.width - 1, size.height - 1));
+    dst_vec.push_back(Point2f(0, size.height - 1));
     namedWindow("Cropped Frame", 0);
 
-    Mat tform = findHomography(pts_src, pts_dst);
-    warpPerspective(im_src, im_dst, tform, size);
+    Mat H = findHomography(src_vec, dst_vec);
+    warpPerspective(src_img, dst_img, H, size);
 
-    imshow("Cropped Frame", im_dst);
+    imshow("Cropped Frame", dst_img);
     waitKey(0);
 
     unsigned int extension_position = img_name.find_last_of('.');
     string cropped_img_name = "";
     cropped_img_name += img_name;
     cropped_img_name.insert(extension_position, "_cropped");
-    imwrite(cropped_img_name, im_dst);
+    imwrite(cropped_img_name, dst_img);
 }
